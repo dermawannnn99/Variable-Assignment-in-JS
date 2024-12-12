eb905e94cb4868ce6a963ed8da0a436c5d4297f7
DERMANRAY <rizkydermawan12345@gmail.com>
2024-12-12 12:30:34 +0700

    Add files via upload

---
+++ variableAssignment(1).js
@@ -0,0 +1,22 @@
+// var JumlahMakanan = 10
+// console.log ('Malam ini saya akan makan ' + JumlahMakanan + ' piring')
+
+// const JumlahPiring = 5
+// console.log ('Malam ini saya akan makan sebanyak ' + JumlahPiring + ' Piring')
+
+// let NamaSaya = 'Menrey' //string
+// let NamaMakanan = 'Gultik' //string
+// const JumlahPiring = 20 //integer
+
+// console.log('Nama saya adalah ' + NamaSaya + ' dan makanan yang saya makan semalam adalah ' + 
+//             NamaMakanan + ' Sebanyak ' + JumlahPiring + ' Piring')
+
+let Nama = 'Menrey'
+let NamaMakanan = 'Gultik'
+let JumlahPiring = 20
+var Harga = 80.125
+
+console.log(
+    `Nama saya adalah ${Nama} , makanan yang saya makan semalam adalah ${NamaMakanan} , sebanyak ${JumlahPiring} piring, 
+    dan harga yang saya harus bayar semuanya sebesar, Rp.${Harga}.`
+)
